7ffd91b12fe6af96119295221c66f88a85b4b565
saltbo <yb1225@qq.com>
2019-02-16 10:30:40 +0800

    add depend_on

--- wallet713.rb
+++ wallet713.rb
@@ -3,10 +3,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Wallet713 < Formula
   desc "A wallet that makes it easy to send, store, and swap Grin."
-  homepage ""
+  homepage "https://github.com/vault713/wallet713"
   url "https://github.com/vault713/wallet713/releases/download/v1.1.0/wallet713-v1.1.0-493635845-osx.tgz"
   sha256 "b9637b813316567e620555eeebfd7309ec343aa82663a641b9022a0f32dd80d0"
-  # depends_on "cmake" => :build
+  depends_on "openssl"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
