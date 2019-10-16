b2762956809b5d23c6888e7bd906a2dfa0fbd2d5
saltbo <yb1225@qq.com>
2019-10-16 14:10:27 +0800

    Update uptoc.rb

--- uptoc.rb
+++ uptoc.rb
@@ -2,10 +2,10 @@
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Uptoc < Formula
-  desc "A cli tool to upload the file for the cloud engine."
+  desc "A cli tool for deploying files to the cloud storage."
   homepage "https://github.com/saltbo/uptoc"
-  url "https://github.com/saltbo/uptoc/releases/download/v1.0.5/uptoc-darwin-amd64.tar.gz"
-  sha256 "b9637b813316567e620555eeebfd7309ec343aa82663a641b9022a0f32dd80d0"
+  url "https://github.com/saltbo/uptoc/releases/download/v1.1.1/uptoc-macos.tar.gz"
+  sha256 "d992ef74dd121533e80962b45c3beffb92c9c56e5d3e97fefef5e993e5227d79"
 
   depends_on :macos => :sierra
 
