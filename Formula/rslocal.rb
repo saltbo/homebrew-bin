6020f24da7bef59e4f90a21f454871ae7934e93c
saltbo <saltbo@foxmail.com>
2022-05-11 22:44:57 +0800

    feat: update the version for rslocal

--- Formula/rslocal.rb
+++ Formula/rslocal.rb
@@ -1,8 +1,8 @@
 class Rslocal < Formula
     desc "A tunnel to localhost built in Rust"
     homepage "https://localtest.rs/"
-    url "https://github.com/saltbo/rslocal/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "b441358c0104d6197da82f6c7e73af95bc28b9bca3df7bcb3bc3b3ee3fd810c7"
+    url "https://github.com/saltbo/rslocal/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "c2346760596a062130227e659cfa9455097f3dff3cc8ae67fe2c907b6cb14028"
     license "Apache-2.0"
   
     depends_on "protobuf" => :build
