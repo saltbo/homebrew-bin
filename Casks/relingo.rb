9b1cdbbe204b253eea565ada91f029bbd5c18a36
saltbo <saltbo@foxmail.com>
2023-03-27 15:17:20 +0000

    Update relingo from 0.0.6 to 0.0.7

--- Casks/relingo.rb
+++ Casks/relingo.rb
@@ -1,9 +1,9 @@
 cask "relingo" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.0.6"
-  sha256 arm:   "b77e2140924b83e3092ce44e3e314862ee7ddd47ae70844847c66c32469e63ec",
-         intel: "4cbebc061f07ee37c9abf226b924215140fb943d5db5de7a646bc39e3f6cd586"
+  version "0.0.7"
+  sha256 arm:   "4e44dfbbdf54b9d410250567f61abc64a4d8972eb2b5ffd60e26066e61e09ff6",
+         intel: "e534fd429712e7ec912613bcf406f85917e711fbd4ae8153777cdaffa8100f59"
 
   url "https://github.com/bonaysoft/relingo-desktop/releases/download/v#{version}/relingo-desktop-v#{version}-macos-#{arch}.dmg"
   name "relingo-desktop"
